84093c1f46ccaf42e952b82a78a685d9f6807b14
Ruymelo10 <ruyovidio@gmail.com>
2021-07-12 14:22:32 -0300

    adding input and buttons to squadbuilder page

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -6,11 +6,16 @@ const STYLES = ['btn--primary', 'btn--outline'];
 
 const SIZES = ['btn--medium', 'btn--large'];
 
-export const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => {
+export const Button = ({ children, type, onClick, buttonStyle, buttonSize, id }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
   return (
-    <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      id={id}
+      onClick={onClick}
+      type={type}
+    >
       {children}
     </button>
   );
@@ -22,6 +27,7 @@ Button.defaultProps = {
 Button.propTypes = {
   buttonSize: P.string,
   buttonStyle: P.string,
+  id: P.string,
   children: P.string.isRequired,
   type: P.string,
   onClick: P.func,
--- src/pages/SquadBuilder/index.jsx
+++ src/pages/SquadBuilder/index.jsx
@@ -1,10 +1,24 @@
 import { React } from 'react';
-import './styles.css';
+import './styles.scss';
+import { Button } from '../../components/Button';
 
 export const SquadBuilder = () => {
   return (
     <div className="SquadBuilder">
-      <h1>Crie seu elenco</h1>
+      <input className="squad-input" placeholder="Dê um nome à sua equipe" />
+      <div className="squad-content">
+        <div className="squad-players"></div>
+        <div className="squad-area"></div>
+        <div className="squad-options">
+          <div className="squad-formations"></div>
+          <div className="squad-buttons">
+            <Button buttonStyle="btn--outline" id="squad-redo-btn">
+              Refazer escalação
+            </Button>
+            <Button buttonStyle="btn--outline">Confirmar escalação</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
